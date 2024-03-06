caa0aa1b8e447de179e0edc8eb3aade48ec0541c
Владимир Чернышов <chiernyshov.vova@list.ru>
2024-03-06 21:03:53 +0300

    Перевел все структуры в классы. Написал конструктор для класса студент. Надо дописать сетеры и гетторы. и дописать автозаполнение из массива значений. написать функцию сортировки по группам в зависимости от оценки.

--- String_Praktik_KSP213-214_PR1/String_Praktik_KSP213-214_PR1.cpp
+++ String_Praktik_KSP213-214_PR1/String_Praktik_KSP213-214_PR1.cpp
@@ -4,6 +4,8 @@
 #include <iostream>
 #include <vector>
 #include <fstream>
+#include <windows.h>
+
 using namespace std;
 class Ses {
 private:
@@ -15,69 +17,76 @@ public:
         this->Name_pred = name_pred;
         this->ocenka = ocenka;
     }
-    void set_Ses() {
-        cout << "название предмета: "; getline(cin >> ws, this->Name_pred);
-        cout << "оценка: "; cin >> this->ocenka;
+    void set_Ses_console() {
+        std::cout << "название предмета: "; getline(std::cin >> ws, this->Name_pred);
+        std::cout << "\nоценка: "; std::cin >> this->ocenka;
     }
-    void out_Ses() {
-        cout << "предмет: " << this->Name_pred << "\tоценка: " << this->ocenka;
+    void out_Ses_console() {
+        std::cout << "предмет: " << this->Name_pred << "\tоценка: " << this->ocenka;
     }
-
+    int get_ocenka() {
+        return this->ocenka;
+    }
 };
-class Person
-{
+
+class Person {
 public:
-    Person() {};
-    ~Person();
-
+    Person() {}
+    Person(std::string name, std::string sername) {
+        this->name = name;
+        this->sername = sername;
+    }
+    void set_Person_console()  {
+        std::cout << "Имя: "; getline(std::cin >> ws, this->name);
+        std::cout << "Фамилия: "; getline(std::cin >> ws, this->sername);
+    }
+    void get_Person_console() {
+        std::cout << "Имя: " << this->name <<"\n";
+        std::cout << "Фамилия: " << this->sername;
+    }
 protected:
-    string Name, Sername;
+    string name, sername;
 
 };
 
-
-
-class Student : Person{
+class Student : Person {
 protected:
     string cod_groupe;
-    int number_groupe , kol_pred =0;
-    Ses* ses = new Ses[kol_pred];// массив из 5 элементов {};
+    int number_groupe, kol_pred;
+    Ses* s;
+    // массив из 5 элементов {};
+public:
+    Student() {}
+    Student(string cod_groupe, int number_groupe, int kol_pred) {
+        this->cod_groupe = cod_groupe;
+        this->number_groupe = number_groupe;
+        in_kol_pred(kol_pred);
+    }
+    void in_kol_pred(int kol_pred) {
+        this->kol_pred = kol_pred;
+        s = new Ses[kol_pred];
+    }
+    void in_Student() {
+        this->set_Person_console();
+        cout << "код группы: "; getline(cin >> ws, this->cod_groupe);
+        cout << "номер группы: "; cin >> this->number_groupe;
+    
+    }
 };
 //student.cod_group = 45 student.izm_cod_grou(5)
-void in_Student(Student &s) {
-    cout << "\nИмя студента: "; getline(cin >> ws, s.Name);
-    cout << "код группы: "; getline(cin >> ws, s.cod_groupe);
-    cout << "номер группы: "; cin >> s.number_groupe;
-    for (int i = 0; i < 5; i++)
-    {
-        s.ses[i].set_Ses();
-    }
-}
+
 int main()
 {
+    SetConsoleCP(1251);// установка кодовой страницы win-cp 1251 в поток ввода
+    SetConsoleOutputCP(1251); // установка кодовой страницы win-cp 1251 в поток вывода
     setlocale(LC_ALL, "RUS");
-   /* int count = 0, j=0, UP=0;
-    string slovo, stroka;
-    cout << "Vvedi simvol" << "\t";
-    getline(cin, slovo);
-    cout << "Vvedi stroku" << "\t";
-    getline(cin, stroka);
-    for (int i = 0; i < stroka.length(); i++) {
-        if (true == isupper(stroka[i]))
-            UP++;
-        if (stroka[i] == slovo[j]) {
-             j++;
-             if (j == slovo.length()) {
-                 count++;
-                 j = 0;
-             }
-        }
-    }
-    cout << count << "\t" << UP;*/
+  
     Ses s;
-    s.set_Ses();
-    Student st, st1[10];
-    //string s;
+    s.set_Ses_console();
+    s.out_Ses_console();
+    Student st{"KSP",213,8}, st1[10];
+    st.in_kol_pred(5);
+   //string s;
     //setlocale(LC_ALL, "Rus");
     //ofstream file;
     //file.open("C:\\Users\\chier\\source\\repos\\String_Praktik_KSP213-214_PR1\\File.txt");
@@ -97,9 +106,24 @@ int main()
     //in.close();
     //struct Phone
     //{
-    for (int i = 0; i < 3; i++) in_Student(st1[i]);
-    in_Student(st);
-
+   /* int count = 0, j=0, UP=0;
+    string slovo, stroka;
+    cout << "Vvedi simvol" << "\t";
+    getline(cin, slovo);
+    cout << "Vvedi stroku" << "\t";
+    getline(cin, stroka);
+    for (int i = 0; i < stroka.length(); i++) {
+        if (true == isupper(stroka[i]))
+            UP++;
+        if (stroka[i] == slovo[j]) {
+             j++;
+             if (j == slovo.length()) {
+                 count++;
+                 j = 0;
+             }
+        }
+    }
+    cout << count << "\t" << UP;*/
    /* int j = 0;
     vector<int> array(100);
     for (int i : array)
@@ -114,14 +138,3 @@ int main()
     }*/
     return 0;
 }
-
-// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
-// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
-
-// Советы по началу работы 
-//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
-//   2. В окне Team Explorer можно подключиться к системе управления версиями.
-//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
-//   4. В окне "Список ошибок" можно просматривать ошибки.
-//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
-//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
